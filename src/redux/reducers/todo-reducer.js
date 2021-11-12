3d229b0bd10764431ece25e1fdb0bc2586c59515
michael belete <it.michael.belete@gmail.com>
2021-11-12 12:42:59 +0300

    created reducer

---
+++ src/redux/reducers/todo-reducer.js
@@ -0,0 +1,54 @@
+import {
+  SET_LOADING,
+  GET_TODOS,
+  GET_TODO,
+  CREATE_TODO,
+  EDIT_TODO,
+  DELETE_TODO,
+  CLEAR_TODO,
+  CHANGE_TODO_STATUS,
+} from "../actions/todo-action";
+
+const initialState = {
+  loading: false,
+  todos: [],
+  todo: null,
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_TODOS:
+      return {
+        ...state,
+        todos: payload,
+        loading: false,
+      };
+    case GET_TODO:
+      return {
+        ...state,
+        todo: payload,
+      };
+
+    case CREATE_TODO:
+      return {
+        ...state,
+        todos: [payload, ...state.todos],
+        loading: false,
+      };
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== payload),
+        loading: false,
+      };
+    
+    default:
+        return state
+  }
+};
