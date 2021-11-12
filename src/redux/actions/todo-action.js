19d8064a0b4a08e90ef495c7ff75ffe71edb161e
michael belete <it.michael.belete@gmail.com>
2021-11-12 12:26:42 +0300

    finished actions

---
+++ src/redux/actions/todo-action.js
@@ -0,0 +1,7 @@
+export const SET_LOADING = "SET_LOADING";
+export const GET_TODOS = "GET_TODOS";
+export const CREATE_TODO = "CREATE_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const EDIT_TODO = "EDIT_TODO";
+export const CLEAR_TODO = "CLEAR_TODO";
+export const CHANGE_TODO_STATUS = "CHANGE_TODO_STATUS";
